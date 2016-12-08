dac0a57881d307d1f8d588f537410df59ddd64c5
david lobo <david@davidlobo.co.uk>
2016-12-08 02:21:21 +0000

    create the landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>MyFooty - Football Fixtures App</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Begin page content -->
+    <div class="container upper-section">
+    <div class="row">
+      <div class="col-sm-9">
+      <div class="page-header">
+        <h1><img class="logo" src="image/football-player-240.png" />MyFooty</h1>
+      </div>
+      <p class="lead">Never miss your favourite football teams live matches again!</p>
+      <p>MyFooty is a simple app for keeping track of your favourite clubs football fixtures. It shows the time, date and TV/Radio station that is broadcasting the live coverage.</p>
+
+      <p>You can select any of the Premier League football teams and whenever you check the app you will be shown your clubs latest fixture data for upcoming games.</p>
+
+      <p>If you have any issues please <a href="mailto:oldlobster@gmail.com?subject=MyFoot">email</a> for support</p>
+
+      <div class="store-button-blue">
+            <a class="w-clearfix w-inline-block" href="https://itunes.apple.com/gb/app/myfooty/id1033037521?mt=8"><img width="150" src="image/app_store_badge.svg" class="store-button">
+            </a>
+      </div>
+
+      </div>
+      <div class="col-sm-3">
+
+      <img class="screenshot img-responsive" src="image/screenshot_iphone_white.png" />
+      </div>
+      </div>
+    </div>
+
+    <!--<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>-->
+
+  </body>
+</html>
